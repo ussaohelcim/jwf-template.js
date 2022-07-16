7b2244bcf6caf0ef41c2535cc71e6a0cb2312c43
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2022-07-15 21:14:47 -0300

    updated

--- game.js
+++ game.js
@@ -2,5 +2,9 @@ let canvas = document.querySelector("#ctx");
 let gfx = jwf(canvas);
 
 setInterval(()=>{
-	
+	gfx.drawCircle({
+		x:canvas.clientWidth/2,
+		y:canvas.clientHeight/2,
+		r:100
+	})
 },16)
